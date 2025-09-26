344e909af5e5baf35316d0f07b251a66a1f9ac01
Hachibitz <felipealmeida.ns@outlook.com>
2025-09-26 13:06:44 -0300

    refactoring and fixes in CryptoConfig for docker environment

--- card-api/src/main/java/br/com/hyperativa/api/config/CryptoConfig.java
+++ card-api/src/main/java/br/com/hyperativa/api/config/CryptoConfig.java
@@ -17,8 +17,8 @@ public class CryptoConfig {
 
     private final PrivateKey privateKey;
     private final PublicKey publicKey;
-    private final static String PRIVATE_KEY_CLASSPATH_LOCATION = "static/KeyPair/private_key.p8";
-    private final static String PUBLIC_KEY_CLASSPATH_LOCATION = "static/KeyPair/public_key.pub";
+    private final static String PRIVATE_KEY_CLASSPATH_LOCATION = "keys/private_key.p8";
+    private final static String PUBLIC_KEY_CLASSPATH_LOCATION = "keys/public_key.pub";
 
     public CryptoConfig() throws Exception {
         this.privateKey = loadPrivateKey();
@@ -34,7 +34,7 @@ public class CryptoConfig {
 
         String privateKeyPEM = new String(keyBytes)
                 .replace("-----BEGIN PRIVATE KEY-----", "")
-                .replaceAll(System.lineSeparator(), "")
+                .replaceAll("\\s", "")
                 .replace("-----END PRIVATE KEY-----", "");
 
         byte[] decodedKey = Base64.getDecoder().decode(privateKeyPEM);
@@ -53,7 +53,7 @@ public class CryptoConfig {
 
         String publicKeyPEM = new String(keyBytes)
                 .replace("-----BEGIN PUBLIC KEY-----", "")
-                .replaceAll(System.lineSeparator(), "")
+                .replaceAll("\\s", "")
                 .replace("-----END PUBLIC KEY-----", "");
 
         byte[] decodedKey = Base64.getDecoder().decode(publicKeyPEM);
